e3340ba56337e28111fd6f33ad28eb8b91f9ea92
scroinkly <amadeusberg1@gmail.com>
2025-02-20 13:51:19 +0100

    Added missing test files for classes and started working on PlayerTest and TileTest

---
+++ src/test/java/ntnu/idatt2003/group27/models/PlayerTest.java
@@ -0,0 +1,36 @@
+package ntnu.idatt2003.group27.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    @DisplayName("Test that creating a new player works.")
+    public void testCreatePlayer(){
+        BoardGame game = new BoardGame();
+        Player player = new Player("Player_1", game);
+        Assertions.assertTrue(player != null);
+    }
+
+    @Test
+    @DisplayName("Test that placeOnTile method functions properly.")
+    public void testPlaceOnTile(){
+        BoardGame game = new BoardGame();
+        Player player = new Player("Player_1", game);
+        Tile tile = new Tile(0);
+        player.placeOnTile(tile);
+        Assertions.assertTrue(player.currentTile == tile);
+
+    }
+
+    @Test
+    @DisplayName("Test that player Move method function properly.")
+    public void testMove(){
+        Board board = new Board();
+        BoardGame game = new BoardGame();
+        Player player = new Player("Player_1", game);
+        player.move(2);
+    }
+}
