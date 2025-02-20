bc4ede7f70c2ced7c409cd91ecebaec1799dc8e4
Alivki <iverlindholm@icloud.com>
2025-02-20 12:41:36 +0100

    landPlayer method logic

--- src/main/java/ntnu/idatt2003/group27/models/Tile.java
+++ src/main/java/ntnu/idatt2003/group27/models/Tile.java
@@ -4,27 +4,27 @@ package ntnu.idatt2003.group27.models;
  * A class that represents a tile on the game board.
  */
 public class Tile {
-    public Tile nextTile;
-    public int tileId;
-    public TileAction landAction;
+  public Tile nextTile;
+  public int tileId;
+  public TileAction landAction;
 
-    public Tile(int tileId){
-        this.tileId = tileId;
-    }
+  public Tile(int tileId) {
+    this.tileId = tileId;
+  }
 
-    public int getTileId(){
-        return tileId;
-    }
+  public int getTileId() {
+    return tileId;
+  }
 
-    public void landPlayer(Player player) {
+  public void landPlayer(Player player) {
+    player.placeOnTile(this);
+  }
 
-    }
+  public void leavePlayer(Player player) {
 
-    public void leavePlayer(Player player){
-        
-    }
+  }
 
-    public void setNextTile(Tile nextTile){
+  public void setNextTile(Tile nextTile) {
 
-    }
+  }
 }
