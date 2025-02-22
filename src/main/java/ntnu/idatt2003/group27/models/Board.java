1f5cb95e7bf65cd66a8d3054d64bf37422e5bb6b
Alivki <iverlindholm@icloud.com>
2025-02-22 13:19:48 +0100

    added error throwing on negative amount of tiles an

--- src/main/java/ntnu/idatt2003/group27/models/Board.java
+++ src/main/java/ntnu/idatt2003/group27/models/Board.java
@@ -11,6 +11,21 @@ public class Board {
   private final Map<Integer, Tile> tiles;
 
   /**
+   * Constructor for the Board class.
+   *
+   * @param numberOfTiles The number of tiles on the board.
+   */
+  public Board(int numberOfTiles) {
+    if (numberOfTiles < 0) {
+      throw new IllegalArgumentException("Number of tiles cannot be negative");
+    }
+
+    this.tiles = new HashMap<Integer, Tile>();
+
+    addTile(numberOfTiles);
+  }
+
+  /**
    * getTiles method to get the tiles of the board.
    *
    * @return The tiles of the board.
@@ -20,22 +35,11 @@ public class Board {
   }
 
   /**
-   * Constructor for the Board class.
-   *
-   * @param numberOfTiles The number of tiles on the board.
-   */
-  public Board(int numberOfTiles) {
-    this.tiles = new HashMap<Integer, Tile>();
-
-    addTile(numberOfTiles);
-  }
-
-  /**
    * Adds the specified number of tiles to the board.
    *
    * @param numberOfTiles The number of tiles to add.
    */
-  public void addTile(int numberOfTiles) {
+  private void addTile(int numberOfTiles) {
     IntStream.range(0, numberOfTiles).forEach(i -> {
       if (tiles.containsKey(i)) {
         throw new IllegalArgumentException("Tile with ID " + i + " already exists");
