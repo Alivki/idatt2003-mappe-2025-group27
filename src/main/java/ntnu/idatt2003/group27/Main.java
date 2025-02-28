11232c02e94878c7ee9166701e1403f83ba57c10
Alivki <iverlindholm@icloud.com>
2025-02-28 17:14:03 +0100

    view folder and boardgameGUI class and gameScreen class to show the first window of the GUI applicatoin

--- src/main/java/ntnu/idatt2003/group27/Main.java
+++ src/main/java/ntnu/idatt2003/group27/Main.java
@@ -4,9 +4,13 @@ import ntnu.idatt2003.group27.models.Board;
 import ntnu.idatt2003.group27.models.BoardGame;
 import ntnu.idatt2003.group27.models.Dice;
 import ntnu.idatt2003.group27.models.Player;
+import ntnu.idatt2003.group27.view.BoardgameGUI;
 
 public class Main {
   public static void main(String[] args) {
+    // to start the GUI for the application. Run main or mvn javafx:run for no error
+    //BoardgameGUI.main(args);
+
     BoardGame game =  new BoardGame(90, 1, 6);
     game.addPlayer(new Player("Alice", game));
     game.addPlayer(new Player("Test", game));
