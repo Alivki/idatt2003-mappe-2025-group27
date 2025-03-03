711ea01ee00c8b78a550c2fbd24ad3997c106540
scroinkly <amadeusberg1@gmail.com>
2025-03-03 15:14:45 +0100

    Created an initial Json file writer, added Gson to pom, added a simple Players.csv file following the structure from the project example, and created a simple JsonFileWriterTest to write board data to file. (can be found in board.json).

---
+++ src/test/java/ntnu/idatt2003/group27/models/JsonFileWriterTest.java
@@ -0,0 +1,21 @@
+package ntnu.idatt2003.group27.models;
+
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import ntnu.idatt2003.group27.filehandler.JsonFileWriter;
+import org.junit.jupiter.api.Test;
+
+public class JsonFileWriterTest {
+  @Test
+  public void WriteBoardToJson(){
+    Board board = new Board(10);
+    JsonFileWriter jsonFileWriter = new JsonFileWriter();
+    JsonObject jsonObject = jsonFileWriter.serializeBoard(board);
+    try {
+      jsonFileWriter.writeFile("Board.Json", jsonObject);
+    }
+    catch (IOException e){
+      System.out.println(e.getMessage());
+    }
+  }
+}
