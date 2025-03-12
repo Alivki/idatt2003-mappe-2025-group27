86d8c839672f22e58e34df760984aad197d199f6
scroinkly <amadeusberg1@gmail.com>
2025-03-12 21:06:15 +0100

    Added a playerCSVFileReader class to read player.csv files. Added a playerCSVFileReaderTest to test this logic. Added player.csv file reading to main game loop.

--- src/main/java/ntnu/idatt2003/group27/BoardGameApp.java
+++ src/main/java/ntnu/idatt2003/group27/BoardGameApp.java
@@ -2,6 +2,7 @@ package ntnu.idatt2003.group27;
 
 import java.io.IOException;
 import ntnu.idatt2003.group27.filehandler.JsonFileReader;
+import ntnu.idatt2003.group27.filehandler.PlayerCsvFileReader;
 import ntnu.idatt2003.group27.models.Board;
 import ntnu.idatt2003.group27.models.BoardGame;
 import ntnu.idatt2003.group27.models.Player;
@@ -29,8 +30,23 @@ public class BoardGameApp {
     else{
       game = new BoardGame(90, 1, 6);
     }
-    game.addPlayer(new Player("Alice", game));
-    game.addPlayer(new Player("Test", game));
+
+    try {
+      PlayerCsvFileReader playerCsvFileReader = new PlayerCsvFileReader();
+      String[][] playerData = playerCsvFileReader.readFile("src/main/java/ntnu/idatt2003/group27/filehandler/Players.csv");
+      for(int i = 0; i < playerData.length; i++){
+        Player player = new Player(playerData[i][0], game);
+        game.addPlayer(player);
+      }
+      System.out.printf("Loaded %d players from file %s", playerData.length, "src/main/java/ntnu/idatt2003/group27/filehandler/Players.csv");
+    }
+    catch (IOException e) {
+      System.out.println(e.getMessage());
+      System.out.println("Error reading Players.csv.");
+      game.addPlayer(new Player("Alice", game));
+      game.addPlayer(new Player("Test", game));
+    }
+
     game.setUpGame();
     game.play();
   }
