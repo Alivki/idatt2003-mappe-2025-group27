3b8b0519dec66c6914240859f039b6e2c7068947
Alivki <iverlindholm@icloud.com>
2025-03-12 21:27:24 +0100

    refactor of JsonFileReader to use the new BoardDeserializer class to get json information and return the Board class

--- src/main/java/ntnu/idatt2003/group27/filehandler/JsonFileReader.java
+++ src/main/java/ntnu/idatt2003/group27/filehandler/JsonFileReader.java
@@ -1,25 +1,24 @@
 package ntnu.idatt2003.group27.filehandler;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.GsonBuilder;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import ntnu.idatt2003.group27.models.Board;
-import ntnu.idatt2003.group27.models.Tile;
 
 /**
  * A class that reads a JSON file and creates a Board object from the contents of the file.
  */
-public class JsonCustomFileReader implements CustomFileReader<Board> {
+public class JsonFileReader implements CustomFileReader<Board> {
 
-  private final Gson gson = new Gson();
+  private final Gson gson;
 
-  public JsonFileReaer() {
-    
+  /**
+   * Creates a new JsonFileReader object.
+   */
+  public JsonFileReader() {
+    this.gson =
+        new GsonBuilder().registerTypeAdapter(Board.class, new BoardDeserializer()).create();
   }
 
   /**
@@ -33,21 +32,10 @@ public class JsonCustomFileReader implements CustomFileReader<Board> {
   @Override
   public Board readFile(String filePath) throws IOException {
     try (FileReader reader = new FileReader(filePath)) {
-      JsonElement jsonElement = JsonParser.parseReader(reader);
-
-      JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-      return gson.fromJson(jsonObject, Board.class);
+      return gson.fromJson(reader, Board.class);
     } catch (Exception e) {
       throw new IOException(e.getMessage());
     }
-  }
-
-  public Board BoardDeserializer(String jsonString) {
-    JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
-    JsonObject tilesJsonObject = jsonObject.getAsJsonObject("tiles");
-
-    List<Tile> tiles = new ArrayList<>();
   }
 }
 
