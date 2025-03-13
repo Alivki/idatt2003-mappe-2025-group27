1aa2bdf15d61deafadd075e851843817b67759ad
Alivki <iverlindholm@icloud.com>
2025-03-13 12:46:19 +0100

    change to adhere to google checkstyle

--- src/main/java/ntnu/idatt2003/group27/models/LadderAction.java
+++ src/main/java/ntnu/idatt2003/group27/models/LadderAction.java
@@ -5,15 +5,14 @@ package ntnu.idatt2003.group27.models;
  */
 public class LadderAction implements TileAction {
 
-    public int destinationTileId;
+  public int destinationTileId;
 
-    public LadderAction(int destinationTileId, String description){
+  public LadderAction(int destinationTileId, String description) {
 
-    }
+  }
 
-    @Override
-    public void Perform(Player player) {
-        
-    }
+  @Override
+  public void Perform(Player player) {
 
+  }
 }
