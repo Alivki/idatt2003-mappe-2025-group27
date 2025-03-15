7ed9567e22a853eb5d0da9ea0253904161f18c75
Iver Lindholm <80721107+Alivki@users.noreply.github.com>
2025-03-15 13:14:58 +0100

    Update TileAction.java
    refactor of javadoc in tile action class

--- src/main/java/ntnu/idatt2003/group27/models/TileAction.java
+++ src/main/java/ntnu/idatt2003/group27/models/TileAction.java
@@ -1,14 +1,20 @@
 package ntnu.idatt2003.group27.models;
 
 /**
- * An interface for custom tile actions.
+ * Defines a contract for custom actions that can be executed when a player interacts with a 
+ * tile on the game board. Implementations of this interface specify behavior triggered by 
+ * a player's presence on a tile.
+ *
+ * @author Iver Lindholm
+ * @version 1.0
+ * @since 1.0
  */
 public interface TileAction {
 
   /**
-   * Method to perform the action of the tile.
+   * Executes the custom action associated with a tile, affecting the specified player.
    *
-   * @param player The player to perform the action on.
+   * @param player The {@link Player} on whom the action is performed.
    */
   void perform(Player player);
 }
