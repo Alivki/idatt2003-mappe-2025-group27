16fa4b5935ff338aad59cc6d8614944ffed893b4
scroinkly <amadeusberg1@gmail.com>
2025-03-20 14:59:48 +0100

    Added methods for adding and removing players from existing csv files

--- src/main/java/ntnu/idatt2003/group27/filehandler/PlayerCsvFileWriter.java
+++ src/main/java/ntnu/idatt2003/group27/filehandler/PlayerCsvFileWriter.java
@@ -1,10 +1,14 @@
 package ntnu.idatt2003.group27.filehandler;
 
+import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
-import com.opencsv.bean.CsvToBean;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 import ntnu.idatt2003.group27.models.Player;
 
 /**
@@ -27,14 +31,30 @@ public class PlayerCsvFileWriter implements CustomFileWriter<Player[]> {
   @Override
   public void writeFile(String filePath, Player[] data) throws IOException {
     try{
-      CSVWriter csvWriter = new CSVWriter(new FileWriter(filePath));
-      String[] header = {"Player_Name", "Player_Piece"};
-      csvWriter.writeNext(header);
+      File playerCsvFile = createPlayerCsvFile(filePath);
+      CSVWriter csvWriter = new CSVWriter(new FileWriter(playerCsvFile, true));
       for (Player player : data) {
         String[] playerInfo = {player.getName(), player.getPiece()};
         csvWriter.writeNext(playerInfo);
       }
       csvWriter.close();
+    } catch (Exception e) {
+      throw new IOException(e);
+    }
+  }
+
+  /**
+   * Creates a default players.csv file in the correct format.
+   * @param filePath
+   * @throws IOException
+   */
+  public File createPlayerCsvFile(String filePath) throws IOException {
+    try{
+      CSVWriter csvWriter = new CSVWriter(new FileWriter(filePath));
+      String[] header = {"Player_Name", "Player_Piece"};
+      csvWriter.writeNext(header);
+      csvWriter.close();
+      return new File(filePath);
     } catch (Exception e) {
       throw new IOException(e);
     }
@@ -48,10 +68,40 @@ public class PlayerCsvFileWriter implements CustomFileWriter<Player[]> {
    */
   public void writePlayerToFile(String filepath, Player player) throws IOException{
     try{
-      CSVWriter csvWriter = new CSVWriter(new FileWriter(filepath));
+
+      File playerCsvFile = new File(filepath);
+      if (!playerCsvFile.exists()) {
+        playerCsvFile = createPlayerCsvFile(filepath);
+      }
+      CSVWriter csvWriter = new CSVWriter(new FileWriter(playerCsvFile, true));
       String[] playerInfo = {player.getName(), player.getPiece()};
       csvWriter.writeNext(playerInfo);
       csvWriter.close();
+    }
+    catch (Exception e) {
+      throw new IOException(e);
+    }
+  }
+
+  /**
+   * Removes the specified player from the specified file.
+   * @param filepath
+   * @param player
+   * @throws IOException
+   */
+  public void removePlayerFromFile(String filepath, Player player) throws IOException, FileNotFoundException {
+    File playerCsvFile = new File(filepath);
+    if (!playerCsvFile.exists()) {
+      throw new FileNotFoundException("File not found");
+    }
+
+    try{
+      PlayerCsvFileReader playerCsvFileReader = new PlayerCsvFileReader();
+      Player[] players = playerCsvFileReader.readFile(filepath);
+      ArrayList<Player> playerList = new ArrayList<>(Arrays.asList(players));
+      playerList.remove(player);
+      players = playerList.toArray(new Player[0]);
+      writeFile(filepath, players);
     }
     catch (Exception e) {
       throw new IOException(e);
