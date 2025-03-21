e27518461496fdc8559f2c6ec1b84a5cce68864e
scroinkly <amadeusberg1@gmail.com>
2025-03-21 21:22:49 +0100

    Updated test

---
+++ src/test/java/ntnu/idatt2003/group27/models/ThrowNewDiceTest.java
@@ -0,0 +1,38 @@
+package ntnu.idatt2003.group27.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A test class for the ThrowNewDice TileAction.
+ * @author Amadeus Berg
+ */
+public class ThrowNewDiceTest {
+  /**
+   * Test the constructor
+   */
+  @Test
+  public void testThrowNewDiceConstructor() {
+    ThrowNewDiceAction throwNewDiceAction = new ThrowNewDiceAction("Throw new dice test description", 1, 6);
+    String expectedDescription = "Throw new dice test description";
+    int expectedNumberOfDice = 1;
+    int expectedNumberOfSides = 6;
+    Assertions.assertEquals(expectedDescription, throwNewDiceAction.description, "Description does not match expected value!");
+    Assertions.assertEquals(expectedNumberOfDice, throwNewDiceAction.numberOfDice, "Number of dice does not match expected value!");
+    Assertions.assertEquals(expectedNumberOfSides, throwNewDiceAction.numberOfDieSides, " Number of sides does not match expected value!");
+  }
+
+  /**
+   * Tests that the perform method of the tileaction functions.
+   */
+  @Test
+  public void testThrowNewDicePerform() {
+    ThrowNewDiceAction throwNewDiceAction = new ThrowNewDiceAction("Throw new dice test description", 1, 6);
+    Board board = new Board(10);
+    Player player = new Player("Test player");
+    int startTileId = 1;
+    player.placeOnTile(board.getTile(startTileId)); //place player on start tile
+    throwNewDiceAction.perform(player);
+    System.out.println("Player landed on tile " + player.getCurrentTile().getTileId());
+  }
+}
