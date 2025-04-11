fc4ad0ed3afb143062fd69451b0ef12ea4a2e83f
Alivki <iverlindholm@icloud.com>
2025-04-11 16:53:59 +0100

    started on implementing ladders

--- src/main/java/ntnu/idatt2003/group27/view/components/Canvas.java
+++ src/main/java/ntnu/idatt2003/group27/view/components/Canvas.java
@@ -16,6 +16,7 @@ import ntnu.idatt2003.group27.models.Tile;
 import ntnu.idatt2003.group27.models.actions.BackToStartAction;
 import ntnu.idatt2003.group27.models.actions.LadderAction;
 import ntnu.idatt2003.group27.models.actions.ThrowNewDiceAction;
+import ntnu.idatt2003.group27.models.interfaces.TileAction;
 
 public class Canvas extends javafx.scene.canvas.Canvas {
   private int boardSize;
@@ -83,19 +84,21 @@ public class Canvas extends javafx.scene.canvas.Canvas {
 
     tileActions.forEach((k, v) -> {
       if (v.getLandAction() != null) {
+        double[] tilePosition = getTilePos(k - 1);
+
         switch (v.getLandAction()) {
           case LadderAction ladderAction -> {
+            double[] tileLandPosition = getTilePos(ladderAction.getDestinationTileId() - 1);
             if (k < ladderAction.getDestinationTileId()) {
               gc.setFill(Color.LIGHTGREEN);
-              double[] tileLandPosition = getTilePos(ladderAction.getDestinationTileId() - 1);
               gc.fillRect(tileLandPosition[0], tileLandPosition[1], tileSize, tileSize);
               gc.setFill(Color.GREEN);
             } else {
               gc.setFill(Color.RED);
-              double[] tileLandPosition = getTilePos(ladderAction.getDestinationTileId() - 1);
               gc.fillRect(tileLandPosition[0], tileLandPosition[1], tileSize, tileSize);
               gc.setFill(Color.DARKRED);
             }
+            drawLadder(gc, k - 1, ladderAction.getDestinationTileId() - 1);
           }
           case BackToStartAction backToStartAction -> {
             gc.setFill(Color.LIGHTPINK);
@@ -108,9 +111,7 @@ public class Canvas extends javafx.scene.canvas.Canvas {
           default -> {break;}
         }
 
-        double[] tilePosition = getTilePos(k - 1);
-
-        gc.fillRect(tilePosition[0], tilePosition[1], tileSize, tileSize);
+        //gc.fillRect(tilePosition[0], tilePosition[1], tileSize, tileSize);
       }
     });
   }
@@ -119,6 +120,36 @@ public class Canvas extends javafx.scene.canvas.Canvas {
     IntStream.range(1, rows + 1).forEach(i -> {
 
     });
+  }
+
+  private void drawLadder(GraphicsContext gc, int tile, int tileLand) {
+    double[] tileCenter = getTileCenter(tile);
+    double[] tileLandCenter = getTileCenter(tileLand);
+
+    gc.setStroke(Color.BLACK);
+    gc.strokeOval(tileLandCenter[0] - 10, tileLandCenter[1] - 10, 20, 20);
+    gc.strokeOval(tileCenter[0] - 10, tileCenter[1] - 10, 20, 20);
+    double[] baseVector = new double[] {100, 0};
+    double[] vector = new double[] {tileLandCenter[0] - tileCenter[0], tileLandCenter[1] - tileCenter[1]};
+
+    int num = (int) (vector[0] * baseVector[0] + vector[1] * baseVector[1]);
+    double den1 = (Math.sqrt(Math.pow(vector[0], 2) + Math.pow(vector[1], 2)));
+    double den2 = (Math.sqrt(Math.pow(vector[0], 2) + Math.pow(vector[1], 2)));
+    double den3 = den1 * den2;
+    double cos = num / den3;
+    double angle = Math.toDegrees(Math.acos(cos));
+
+    int radius = 20;
+    double[] firstLadderLegPointOne = new double[] {radius * Math.sin(angle), radius * Math.cos(angle)};
+    double[] firstLadderLegPointTwo = new double[] {radius * Math.sin(-angle), radius * Math.cos(-angle)};
+
+    gc.setStroke(Color.PURPLE);
+    gc.beginPath();
+    gc.moveTo(tileCenter[0] + firstLadderLegPointOne[0], tileCenter[1] + firstLadderLegPointOne[1]);
+    gc.lineTo(tileLandCenter[0] - firstLadderLegPointOne[0], tileLandCenter[1] + firstLadderLegPointOne[1]);
+    gc.moveTo(tileLandCenter[0] + firstLadderLegPointTwo[0], tileLandCenter[1] - firstLadderLegPointTwo[1]);
+    gc.lineTo(tileCenter[0] + firstLadderLegPointTwo[0], tileCenter[1] + firstLadderLegPointTwo[1]);
+    gc.stroke();
   }
 
   private void drawPlayers(GraphicsContext gc) {
@@ -149,12 +180,12 @@ public class Canvas extends javafx.scene.canvas.Canvas {
   private double[] getTilePos(int tileId) {
     double yPos = (tileSize * rows) - tileSize;
 
-    int row = tileId % columns;
-    int col = tileId / columns;
+    int col = tileId % columns;
+    int row = tileId / columns;
 
-    boolean leftToRight = (col % 2 == 0);
-    double xPos = leftToRight ? row * tileSize : (columns - 1 - row) * tileSize;
-    double currentYPos = yPos - (col * tileSize);
+    boolean leftToRight = (row % 2 == 0);
+    double xPos = leftToRight ? col * tileSize : (columns - 1 - col) * tileSize;
+    double currentYPos = yPos - (row * tileSize);
 
     return new double[] {xPos, currentYPos};
   }
@@ -162,12 +193,12 @@ public class Canvas extends javafx.scene.canvas.Canvas {
   private double[] getTileCenter(int tileId) {
     double yPos = (tileSize * rows) - tileSize;
 
-    int row = tileId % columns;
-    int col = tileId / columns;
+    int col = tileId % columns;
+    int row = tileId / columns;
 
-    boolean leftToRight = (col % 2 == 0);
-    double xPos = leftToRight ? row * tileSize : (columns - 1 - row) * tileSize;
-    double currentYPos = yPos - (col * tileSize);
+    boolean leftToRight = (row % 2 == 0);
+    double xPos = leftToRight ? col * tileSize : (columns - 1 - col) * tileSize;
+    double currentYPos = yPos - (row * tileSize);
 
     return new double[] {xPos + (tileSize / 2), currentYPos + (tileSize / 2)};
   }
