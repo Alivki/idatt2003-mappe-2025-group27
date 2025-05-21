07415e851d69979b7bb8afe4c73498ae450fa5a5
scroinkly <amadeusberg1@gmail.com>
2025-05-21 21:14:14 +0200

    Made JsonLadderGameConfiguration extend LadderGameConfiguration to remove redundancy

--- src/main/java/ntnu/idatt2003/group27/models/JsonLadderGameConfiguration.java
+++ src/main/java/ntnu/idatt2003/group27/models/JsonLadderGameConfiguration.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
+import ntnu.idatt2003.group27.models.enums.LadderGameType;
 import ntnu.idatt2003.group27.models.interfaces.GameConfiguration;
 import ntnu.idatt2003.group27.models.interfaces.TileAction;
 import ntnu.idatt2003.group27.utils.filehandler.json.JsonFileReader;
@@ -13,11 +14,11 @@ import ntnu.idatt2003.group27.utils.filehandler.json.JsonFileReader;
  * {@link GameConfiguration} interface to provide game settings, including the number of dice, die
  * sides, total tiles, and tile actions, based on the JSON configuration.
  *
- * @author Iver Lindholm
+ * @author Iver Lindholm, Amadeus Berg
  * @version 1.0
  * @since 2.0
  */
-public class JsonLadderGameConfiguration implements GameConfiguration {
+public class JsonLadderGameConfiguration extends LadderGameConfiguration {
   /**
    * Logger instance for the {@link JsonLadderGameConfiguration} class.
    * Used for logging informational messages and errors related to class operations.
@@ -29,13 +30,15 @@ public class JsonLadderGameConfiguration implements GameConfiguration {
   private Board board;
 
   /**
-   * Constructs a {@code JsonLadderGameConfiguration} by reading the game board configuration from
+   * Constructs a {@link JsonLadderGameConfiguration} by reading the game board configuration from
    * the specified JSON file.
    *
    * @param jsonPath The file path to the JSON configuration file.
    * @throws IOException if the JSON file cannot be read or is invalid.
    */
+
   public JsonLadderGameConfiguration(String jsonPath) throws IOException {
+    super(LadderGameType.JSON);
     logger.fine("Initializing JsonLadderGameConfiguration for path " + jsonPath);
     this.jsonPath = jsonPath;
 
@@ -45,36 +48,6 @@ public class JsonLadderGameConfiguration implements GameConfiguration {
       logger.severe("Error reading JsonLadderGameConfiguration for path " + jsonPath);
       throw new IOException(e.getMessage());
     }
-  }
-
-  @Override
-  public int getNumberOfDice() {
-    logger.fine("Getting number of dice.");
-    return 1;
-  }
-
-  @Override
-  public int getNumberOfDieSides() {
-    logger.fine("Getting number of dice sides.");
-    return 6;
-  }
-
-  @Override
-  public int getTotalTiles() {
-    logger.fine("Getting number of tiles.");
-    return board.getTiles().size();
-  }
-
-  @Override
-  public Map<Integer, TileAction> getTileActions() {
-    logger.fine("Getting tile actions.");
-    Map<Integer, TileAction> tileActions = new HashMap<>();
-    board.getTiles().forEach((id, tile) -> {
-      if (tile.getLandAction() != null) {
-        tileActions.put(id, tile.getLandAction());
-      }
-    });
-    return tileActions;
   }
 
   /**
