12dd6474f2be65df2f446dca949f1b6d719ba674
Alivki <iverlindholm@icloud.com>
2025-05-21 21:30:32 +0200

    json file writer test compltet

--- src/test/java/ntnu/idatt2003/group27/models/JsonFileWriterTest.java
+++ src/test/java/ntnu/idatt2003/group27/models/JsonFileWriterTest.java
@@ -1,38 +1,120 @@
 package ntnu.idatt2003.group27.models;
 
-import com.google.gson.JsonObject;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.IntStream;
 
+import ntnu.idatt2003.group27.models.actions.LadderAction;
 import ntnu.idatt2003.group27.utils.filehandler.json.JsonFileWriter;
 import org.junit.jupiter.api.BeforeEach;
+import java.io.StringWriter;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test class for the {@link JsonFileWriter} class.
+ */
 public class JsonFileWriterTest {
-  Map<Integer, Tile> tiles;
-
+  private JsonFileWriter jsonFileWriter;
+  private StringWriter stringWriter;
 
   /**
-   * Creates a map of tiles for the board.
+   * Sets up the test environment by initializing a {@link JsonFileWriter} instance,
+   * creating a {@link Board} with 10 tiles,
+   * and preparing a {@link StringWriter} for output.
    */
   @BeforeEach
-  public void createTiles() {
-    tiles = new HashMap<>();
-    IntStream.range(0, 10).forEach(i -> tiles.put(i, new Tile(i)));
+  public void setUp() {
+    jsonFileWriter = new JsonFileWriter();
+    stringWriter = new StringWriter();
   }
 
+  /**
+   * Test serialization of a board with a single tile to JSON string.
+   */
   @Test
-  public void WriteBoardToJsonTest(){
-    Board board = new Board(tiles);
-    JsonFileWriter jsonFileWriter = new JsonFileWriter();
+  public void writeFileWithSingleTileSerializesCorrectly() throws IOException {
+    Map<Integer, Tile> tiles = new HashMap<>();
+    Tile tile = new Tile(1);
+    tiles.put(1, tile);
 
-    try {
-      jsonFileWriter.writeFile("Board.Json", board);
-    }
-    catch (IOException e){
-      System.out.println(e.getMessage());
-    }
+    Board board = new Board(tiles);
+
+    jsonFileWriter.writeFile("path", board);
+    Gson gson = new Gson();
+    gson.toJson(jsonFileWriter.gson.toJsonTree(board), stringWriter);
+
+    String jsonOutput = stringWriter.toString();
+    assertTrue(jsonOutput.contains("\"tiles\""));
+    assertTrue(jsonOutput.contains("\"id\":1"));
+    assertFalse(jsonOutput.contains("\"nextTileId\""));
+    assertFalse(jsonOutput.contains("\"action\""));
   }
+
+  /**
+   * Test serialization of a board with a tile and ladderAction.
+   */
+  @Test
+  void writeFileWithLadderActionSerializesCorrectly() throws IOException {
+    Map<Integer, Tile> tiles = new HashMap<>();
+    Tile tile = new Tile(1);
+    tile.setLandAction(new LadderAction(5, "Go to tile 5"));
+    tiles.put(1, tile);
+
+    Board board = new Board(tiles);
+
+    jsonFileWriter.writeFile("path", board);
+    Gson gson = new Gson();
+    gson.toJson(jsonFileWriter.gson.toJsonTree(board), stringWriter);
+    System.out.println(stringWriter.toString());
+
+    String jsonOutput = stringWriter.toString();
+    assertTrue(jsonOutput.contains("\"type\":\"LadderAction\""));
+    assertTrue(jsonOutput.contains("\"targetTile\":5"));
+    assertTrue(jsonOutput.contains("\"description\":\"Go to tile 5\""));
+  }
+
+  /**
+   * Test serialization of a board with no tiles.
+   */
+  @Test
+  void writeFileWithEmptyBoardSerializesCorrectly() throws IOException {
+    Map<Integer, Tile> tiles = new HashMap<>();
+    Board board = new Board(tiles);
+
+    jsonFileWriter.writeFile("path", board);
+    Gson gson = new Gson();
+    gson.toJson(jsonFileWriter.gson.toJsonTree(board), stringWriter);
+
+    String jsonOutput = stringWriter.toString();
+    assertTrue(jsonOutput.contains("\"tiles\""));
+    assertTrue(jsonOutput.contains("[]"));
+  }
+
+  /**
+   * Test serialization of a tile with null action.
+   */
+  @Test
+  void writeFileWithNullActionSerializesCorrectly() throws IOException {
+    Map<Integer, Tile> tiles = new HashMap<>();
+    Tile tile = new Tile(1);
+    tile.setLandAction(null);
+    tiles.put(1, tile);
+
+    Board board = new Board(tiles);
+
+    jsonFileWriter.writeFile("path", board);
+    Gson gson = new Gson();
+    gson.toJson(jsonFileWriter.gson.toJsonTree(board), stringWriter);
+
+    String jsonOutput = stringWriter.toString();
+    assertTrue(jsonOutput.contains("\"tiles\""));
+    assertTrue(jsonOutput.contains("\"id\":1"));
+    assertFalse(jsonOutput.contains("\"nextTileId\""));
+    assertFalse(jsonOutput.contains("\"action\""));
+  }
+
 }
